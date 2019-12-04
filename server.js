7dffb640f1ae52206b3471a4d78a5c705c6fb31b
Arjun Raja <arjunraja@cybrilla.com>
2019-12-04 19:30:17 +0530

    added user model

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const app = express();
 
+<<<<<<< Updated upstream
 app.get('/', (req,res)=> {res.send('hello world')})
+=======
+
+// init Middlewar
+app.use(express.json({extended : true}));
+
+app.get('/', (req,res)=> {res.send('hello world')});
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+>>>>>>> Stashed changes
 
 const PORT = process.env.PORT || 5000;
 
